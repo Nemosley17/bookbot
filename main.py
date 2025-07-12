ab61e4b8919044bc438b5c05babb329b99f1b4bc
Nemosley17 <nicholas.mosley17@gmail.com>
2025-07-11 22:48:40 -0400

    updated functions, and added two books

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-from stats import word_count
+from stats import string_count
+from stats import letter_count
+from stats import sort_character_count
+import sys
 
-
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+    
+filepath = sys.argv[1]
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-
-
-
-
-
-       
 def main():
-    book_text = get_book_text("/home/nemosley17/bookbot/books/frankenstein.txt")
-    count_words = word_count(book_text)
-    print(f"{count_words} words found in the document")
+    book_text = get_book_text(filepath)
+    word_count = string_count(book_text)
+    character_count = letter_count(book_text)
+    sorted_chars = sort_character_count(character_count)  # Use character_count instead of char_count_dict
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+
+    print("--------- Character Count -------")
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["count"]
+
+        if char.isalpha():
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
     
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,23 @@
-def word_count(text):
+def string_count(text):
     words = text.split()
     return len(words)
+
+
+def letter_count(text):
+    text = text.lower()
+    counts = {}
+    
+    for char in text:
+        counts[char] = counts.get(char, 0) + 1
+    return counts
+
+def sort_character_count(char_dict):
+    sorted_chars = []
+    
+    for char, count in char_dict.items():
+        sorted_chars.append({"char": char, "count": count})
+    
+    def sort_on(dict):
+        return dict["count"]
+    sorted_chars.sort(reverse = True, key = sort_on)
+    return sorted_chars    
